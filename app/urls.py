f9a034b320e6b041d50185bff6e45350dfa53573
Pujan Maharjan <pujanmaharjan22@gmail.com>
2024-07-08 22:44:16 +0545

    added homepage and tags and view count on post

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post/<slug:slug>', views.post_page, name='post_page')
+    path('', views.index, name='index'),
+    path('post/<slug:slug>', views.post_page, name='post_page'),
 ]
