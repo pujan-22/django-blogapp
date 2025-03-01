676ef26b442f5de363ce7aab415fa39596ca4794
Pujan Maharjan <pujanmaharjan22@gmail.com>
2025-03-01 23:26:15 +0545

    Authentication

--- app/views.py
+++ app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from app.forms import CommentForm, SubscribeForm
+from app.forms import CommentForm, SubscribeForm, NewUserForm
 from app.models import Post, Comments, Profile, Tag, WebsiteMeta
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.db.models import Count
+from django.contrib.auth import login
 
 # Create your views here.
 def index(request):
@@ -27,6 +28,7 @@ def index(request):
         subscribe_form = SubscribeForm(request.POST)
         if subscribe_form.is_valid():
             subscribe_form.save()
+            request.session['subscribed'] = True
             subscribe_success = 'Subscribed Successfully'
             subscribe_form = SubscribeForm()
 
@@ -109,3 +111,16 @@ def about(request):
         website_info = WebsiteMeta.objects.all()[0]
     context = {'website_info':website_info}
     return render(request, 'app/about.html' ,context)
+
+
+def register_user(request):
+    form = NewUserForm()
+    if(request.method == "POST"):
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+        
+    context = {'form' : form}
+    return render(request, 'registration/register.html', context)
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('app.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
